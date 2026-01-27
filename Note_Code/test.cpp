2bf8d35246d3f22bda05163ad05f550127f82487
xingzi <xingzi1019@qq.com>
2026-01-27 16:06:36 +0800

    1.27.1

--- Note_Code/test.cpp
+++ Note_Code/test.cpp
@@ -306,8 +306,62 @@
 //		cout << "a + b 不等于 c" << endl;
 //	return 0;
 //}
-int main()
-{
-	cout << 'X' << 'Z' << endl;
-	return 0;
-}
+//学生结构体定义
+//struct student
+//{
+//	//成员列表
+//	string name;  //姓名
+//	int age;      //年龄
+//	int score;    //分数
+//};
+////值传递
+//void printStudent(student stu)
+//{
+//	stu.age = 28;
+//	cout << "子函数中 姓名：" << stu.name << " 年龄： " << stu.age << " 分数：" << stu.score << endl;
+//}
+////地址传递
+//void printStudent2(student* stu)
+//{
+//	stu->age = 28;
+//	cout << "子函数中 姓名：" << stu->name << " 年龄： " << stu->age << " 分数：" << stu->score << endl;
+//}
+//int main() {
+//	student stu = { "张三",18,100 };
+//	//值传递
+//	printStudent(stu);
+//	cout << "主函数中 姓名：" << stu.name << " 年龄： " << stu.age << " 分数：" << stu.score << endl;
+//	cout << endl;
+//	//地址传递
+//	printStudent2(&stu);
+//	cout << "主函数中 姓名：" << stu.name << " 年龄： " << stu.age << " 分数：" << stu.score << endl;
+//	system("pause");
+//	return 0;
+//}
+//学生结构体定义
+//struct student
+//{
+//	//成员列表
+//	string name;  //姓名
+//	int age;      //年龄
+//	int score;    //分数
+//};
+//
+////const使用场景
+//void printStudent(const student* stu) //加const防止函数体中的误操作
+//{
+//	//stu->age = 100; //操作失败，因为加了const修饰
+//	cout << "姓名：" << stu->name << " 年龄：" << stu->age << " 分数：" << stu->score << endl;
+//
+//}
+//
+//int main() {
+//
+//	student stu = { "张三",18,100 };
+//
+//	printStudent(&stu);
+//
+//	system("pause");
+//
+//	return 0;
+//}
