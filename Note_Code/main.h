5ce14599cf5d7629d65ea1cac40318e110b47e79
xingzi <xingzi1019@qq.com>
2026-03-14 22:53:23 +0800

    3.14.22.53

--- Note_Code/main.h
+++ Note_Code/main.h
@@ -1,12 +1,12 @@
 ﻿#pragma once
 //头文件
+#include <algorithm>//max min
 #include <cmath>
 #include <cstdio>
+#include <ctime>
 #include <iomanip>//(IO manipulators)头⽂件中的操纵符
 #include <iostream>
 #include <string>
 #include <utility>
-#include <ctime>
-#include <algorithm>//max min
 #include <vector>
 
